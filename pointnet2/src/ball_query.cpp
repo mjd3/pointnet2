6d4485720bc4d4639742e8cbae2791e9e4a0a2e6
Mike Danielczuk <michael.danielczuk@gmail.com>
2021-04-16 20:31:37 -0700

    device cuda check fix

--- pointnet2/src/ball_query.cpp
+++ pointnet2/src/ball_query.cpp
@@ -13,7 +13,7 @@
 
 extern THCState *state;
 
-#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x, " must be a CUDAtensor ")
+#define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x, " must be a CUDAtensor ")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x, " must be contiguous ")
 #define CHECK_INPUT(x) \
     CHECK_CUDA(x);     \
